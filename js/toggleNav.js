d74dc80a2eba69fe0cbc801d245d84274c83a72e
lhourquin <lhourquin@gmail.com>
2020-02-17 11:48:43 +0100

    start script for white button burger in index page

--- js/toggleNav.js
+++ js/toggleNav.js
@@ -3,21 +3,25 @@ let buttonBurger = document.getElementById('button-burger');
 let header = document.getElementById('header');
 let nav = document.getElementById('header-nav');
 let md = window.matchMedia("(max-width: 1050px)");
+let main = document.getElementById('main');
 
-
+// function for media query active
 function mediaScreen (mq) {
     if(mq.matches){
         header.classList.add('close');
         nav.classList.add('displayNone');
+        buttonBurger.classList.remove('displayNone');
     }else{
         header.classList.remove('close');
         nav.classList.remove('displayNone');
+        buttonBurger.classList.add('displayNone');
     }
 }
 
 mediaScreen(md);
 md.addListener(mediaScreen);
 
+//function for toggle nav bar
 function toggle (e) {
     let target = e.target;
     
@@ -25,7 +29,30 @@ function toggle (e) {
         console.log('ok');
         header.classList.toggle('close');
         nav.classList.toggle('displayNone');
+        main.classList.toggle('displayNone');
+        buttonBurger.classList.toggle('btnBlack');
+
     }
 }
 
 buttonBurger.addEventListener('click', toggle);
+
+
+//function for check  page
+function checkThePage (){
+    var checkPage = window.location.pathname;
+
+if (checkPage.length === 0 || checkPage === "/" || checkPage.match(/^\/?index/)){
+
+    
+    if(!header.classList.contains('close')){
+        buttonBurger.classList.remove('btnWhite');
+    }/*else if(header.classList.contains('btnBlack')) {
+        buttonBurger.classList.add('btnWhite');
+
+    }*/
+}
+
+}
+
+checkThePage();
