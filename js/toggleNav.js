bc64bcfd736d9a01cdc7aae01bd964835ca6f537
Lhourquin <lhourquin@gmail.com>
2020-02-18 19:35:42 +0100

    resolve bug, found solution for btn burger white when i on index

--- js/toggleNav.js
+++ js/toggleNav.js
@@ -4,6 +4,7 @@ let header = document.getElementById('header');
 let nav = document.getElementById('header-nav');
 let md = window.matchMedia("(max-width: 1050px)");
 let main = document.getElementById('main');
+let body = document.getElementsByTagName('body')[0];
 
 // function for media query active
 function mediaScreen (mq) {
@@ -11,7 +12,13 @@ function mediaScreen (mq) {
         header.classList.add('close');
         nav.classList.add('displayNone');
         buttonBurger.classList.remove('displayNone');
-        checkThePage();
+        if (body.classList.contains('body-index')){
+            //if(header.classList.contains('close')){
+                buttonBurger.classList.add('btnWhite');
+            //}
+
+        }
+      //  checkThePage();
 
     }else{
         header.classList.remove('close');
@@ -32,30 +39,17 @@ function toggle (e) {
         header.classList.toggle('close');
         nav.classList.toggle('displayNone');
         main.classList.toggle('displayNone');
-        buttonBurger.classList.toggle('btnWhite');
+        if (body.classList.contains('body-index')){
+            if(!header.classList.contains('close')){
+                buttonBurger.classList.remove('btnWhite');
+            }else if(header.classList.contains('close')){
+                buttonBurger.classList.add('btnWhite');
+
+            }
+
+        }
 
     }
 }
 
 buttonBurger.addEventListener('click', toggle);
-
-
-//function for check  page
-function checkThePage (){
-    var checkPage = window.location.pathname;
-
-if (checkPage.length === 0 || checkPage === "/" || checkPage.match(/^\/?Livrasion2\/index/)){
-
-    
-    if(header.classList.contains('close')){
-        buttonBurger.classList.add('btnWhite');
-    }/*else if(header.classList.contains('btnBlack')) {
-        buttonBurger.classList.add('btnWhite');
-
-    }*/
-}else if (checkPage.length === 0 || checkPage === "/" || checkPage.match(/^\/?contact/) || checkPage.match(/^\/?prestation/) || checkPage.match(/^\/?a-propos/)) {
-    buttonBurger.style.color = "black";
-}
-
-}
-
